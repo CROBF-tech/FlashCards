33fa95c5219b4c73abbf0899ed31f690cdf34468
yoezequiel <ezequielcasiano15@gmail.com>
2025-05-29 18:32:21 -0300

    add website in brach new

--- frontend/config.js
+++
@@ -1,9 +0,0 @@
-const DEV_API_URL = 'http://192.168.100.5:8000';
-const PROD_API_URL = ''; // Aquí irá la URL de producción cuando la tengamos
-
-export const API_URL = __DEV__ ? DEV_API_URL : PROD_API_URL;
-
-export const CONFIG = {
-    tokenStorageKey: 'flashcards_token',
-    tokenHeaderKey: 'x-auth-token',
-};
