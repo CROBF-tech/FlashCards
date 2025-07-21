ef991a2b80e98587cda64053551509be49b7038d
yoezequiel <ezequielcasiano15@gmail.com>
2025-07-21 15:41:48 -0300

    Fix: Eliminar barra diagonal final de la URL de producción en la configuración de la API

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 const DEV_API_URL = 'http://192.168.100.5:8000';
-const PROD_API_URL = 'https://flash-cards-orcin-omega.vercel.app/';
+const PROD_API_URL = 'https://flash-cards-orcin-omega.vercel.app';
 
 export const API_URL = __DEV__ ? DEV_API_URL : PROD_API_URL;
 
